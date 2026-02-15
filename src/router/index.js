08d0a8ef3ebeb6d0cb7bfdc3c9906ff28d83cbc8
Olayimika001 <ymariam897@gmail.com>
2026-02-15 14:27:36 -0500

    add github pages router fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   { 
@@ -52,8 +52,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory()
-,
+  history: createWebHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     // If there's a saved position (browser back/forward), use it
